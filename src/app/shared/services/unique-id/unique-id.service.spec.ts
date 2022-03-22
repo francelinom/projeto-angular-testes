2227e40aef53fd0c6eda61f63919c0dd36e43c83
francelino <fmarcilio1989@gmail.com>
2022-03-21 21:42:16 -0300

    Adicionando o forEach para evitar que o jamine fiquei todando aleatoriamente

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,19 +1,29 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
+  let service: UniqueIdService = null;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
   it(`#${UniqueIdService.prototype.genereteUniqueIdWithPrefix.name} should generate id when called with preix`, () => {
-    const service = new UniqueIdService();
     const id = service.genereteUniqueIdWithPrefix('app');
 
     expect(id.startsWith('app-')).toBeTrue();
   });
 
   it(`#${UniqueIdService.prototype.genereteUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-    const service = new UniqueIdService();
     const ids = new Set();
     for (let i = 0; i < 50; i++) {
       ids.add(service.genereteUniqueIdWithPrefix('app'));
     }
     expect(ids.size).toBe(50);
+  });
+
+  it(`${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} sholud return the number of generatedIds when called`, () => {
+    service.genereteUniqueIdWithPrefix('app');
+    service.genereteUniqueIdWithPrefix('app');
+
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
   });
 });
