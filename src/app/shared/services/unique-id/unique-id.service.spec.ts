d5cdaaab9f82a51e95a00da926b0c861473cd92d
francelino <fmarcilio1989@gmail.com>
2022-03-21 21:01:40 -0300

    Adicionando o template string para remover erros de mudanças de nomes de class e metodos

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,7 +1,7 @@
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
-  it('#genereteUniqueIdWithPrefix should generate id when called with preix', () => {
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.genereteUniqueIdWithPrefix.name} should generate id when called with preix`, () => {
     const service = new UniqueIdService();
     const id = service.genereteUniqueIdWithPrefix('app');
 
